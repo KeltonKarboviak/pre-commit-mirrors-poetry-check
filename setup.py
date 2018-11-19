c4ab3702ddbaa7b31cd7881548a3bcf9b6f47509
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-11-19 01:24:57 +0000

    Mirror: 0.8.0

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['poetry==0.8.0a4'],
+    install_requires=['poetry==0.8.0'],
 )
