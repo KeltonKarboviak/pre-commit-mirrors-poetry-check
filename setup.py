be739a93babbfedd11f5b727791d189b27cab9a8
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-11-19 01:24:58 +0000

    Mirror: 0.9.0a1

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['poetry==0.9.0a0'],
+    install_requires=['poetry==0.9.0a1'],
 )
